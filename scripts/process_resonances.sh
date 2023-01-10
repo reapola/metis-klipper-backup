2e7b40ad87244263f4d2c82fa4a9f1d16a6935c1
reapola <36609013+reapola@users.noreply.github.com>
2023-01-10 22:35:16 +0000

    procres

--- scripts/process_resonances.sh
+++ scripts/process_resonances.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 now=$(date +"%F-%H_%M_%S")
 
-#echo Doing a full probe accuracy test
-#python3 $HOME/probe_accuracy_tests/probe_accuracy_test_suite.py
+echo Doing a full probe accuracy test
+python3 $HOME/probe_accuracy_tests/probe_accuracy_test_suite.py
 
 echo Moving Files
 mv /home/reapola/probe_accuracy_tests/output/* /home/reapola/klipper_config/probe_accuracy/
